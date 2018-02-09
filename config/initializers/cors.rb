888392d39f2158d69af7f68a0a2610118472210d
szorfein <szorfein@gmail.com>
2018-02-09 13:16:42 +0100

    install racks-cors

---
+++ config/initializers/cors.rb
@@ -0,0 +1,11 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+        origins 'localhost:3000','127.0.0.1:3000',
+            /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
+
+        resource '*',
+            headers: :any,
+            expose:  ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+            methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    resources :people, except: [:show]
+    resources :people, except: [:show], constraints: { id: /[a-f0-9]{24}+/ }
 
     root to: "people#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
